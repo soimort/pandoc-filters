d577f73ed8998baa57d209a1bc7b55d0c76f48d3
Mort Yao <soi@mort.ninja>
2017-09-04 16:11:29 +0200

    qed.hs: support raw LaTeX "\Qed"

--- qed.hs
+++ qed.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env runhaskell
 {-
-  Pandoc filter to convert end-of-paragraph string "[QED]"
+  Pandoc filter to convert raw LaTeX "\Qed" or end-of-paragraph string "[QED]"
   into tombstone symbol. (HTML only)
 -}
 import Text.Pandoc.JSON
@@ -12,15 +12,19 @@ d = hPutStrLn stderr . show
 fmt :: Format
 fmt = Format "html"
 
+htmlTombstone :: String
+htmlTombstone =
+  "<p style='text-align:right !important;" ++
+  "text-indent:0 !important;" ++
+  "position:relative;top:-1em'>&#9632;</p>"
+
 qed :: Block -> IO Block
-qed b@(Para ins) =
-  case last ins of
-    Str "[QED]" ->
-      return $ Para $ init ins ++ [ RawInline fmt $
-                                    "<p style='text-align:right !important;" ++
-                                    "text-indent:0 !important;" ++
-                                    "position:relative;top:-1em'>&#9632;</p>" ]
-    _ -> return b
+qed b@(RawBlock (Format "latex") "\\Qed") =
+  return $ RawBlock fmt htmlTombstone
+qed b@(Para ins) | last ins == Str "[QED]" =
+                     return $ Para $ init ins ++ [
+                     RawInline fmt htmlTombstone ]
+                 | otherwise = return b
 qed x = return x
 
 main :: IO ()
